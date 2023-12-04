2b5bd25b061bb2d11a60056fbd1523a28036a78a
Evan Rice <evancr2017@gmail.com>
2023-12-03 20:59:24 -0600

    made some beginner routes and session key

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,25 @@
 const router = require("express").Router();
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
     res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/charChoice", withAuth, async (req, res) => {
+  try {
+    res.render("");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/start", withAuth, async (req, res) => {
+  try {
+    res.render("");
   } catch (err) {
     res.status(500).json(err);
   }
