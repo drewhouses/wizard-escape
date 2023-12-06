db7941747ccd625ab0cdd2f5d7c9293250d154f2
MultiMouse <olmos.luisd@outlook.com>
2023-12-05 21:44:20 -0600

    saved changes to handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const withAuth = require("../utils/auth");
+const { Player } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -26,5 +27,13 @@ router.get("/game/dungeon", (req, res) => {
     logged_in: req.session.logged_in,
   });
 });
+
+// router.get("/game/dungeon/:id", async (req, res) => {
+//   const playerData = await Player.findByPk(req.params.id);
+//   const playerInfo = playerData.get({ plain: true });
+//   res.render("room", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
 
 module.exports = router;
