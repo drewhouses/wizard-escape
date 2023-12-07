6e2488351605d3d515672cff7b9f6693a2698e31
MultiMouse <olmos.luisd@outlook.com>
2023-12-07 17:40:55 -0600

    added inventory and action history stuff, and avatar and name display

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,34 +16,79 @@ router.get("/start", (req, res) => {
   });
 });
 
-router.get("/game", (req, res) => {
+router.get("/game/:id", async (req, res) => {
+  const playerData = await Player.findByPk(req.params.id);
+  const player = playerData.get({ plain: true });
   res.render("intro", {
+    player,
     logged_in: req.session.logged_in,
   });
-});
+}); //swap with bottom if not work
 
-router.get("/game/dungeon", (req, res) => {
+// router.get("/game", (req, res) => {
+//   res.render("intro", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
+
+router.get("/game/dungeon/:id", async (req, res) => {
+  const playerData = await Player.findByPk(req.params.id);
+  const player = playerData.get({ plain: true });
   res.render("room", {
+    player,
     logged_in: req.session.logged_in,
   });
-});
+}); //swap with bottom if not work
 
-router.get("/game/library", (req, res) => {
+// router.get("/game/dungeon", (req, res) => {
+//   res.render("room", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
+
+router.get("/game/library/:id", async (req, res) => {
+  const playerData = await Player.findByPk(req.params.id);
+  const player = playerData.get({ plain: true });
   res.render("library", {
+    player,
     logged_in: req.session.logged_in,
   });
-});
+}); //swap with bottom if not work
 
-router.get("/game/escape", (req, res) => {
+// router.get("/game/library", (req, res) => {
+//   res.render("library", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
+
+router.get("/game/escape/:id", async (req, res) => {
+  const playerData = await Player.findByPk(req.params.id);
+  const player = playerData.get({ plain: true });
   res.render("escape-room", {
+    player,
     logged_in: req.session.logged_in,
   });
-});
+}); //swap with bottom if not work
 
-router.get("/game/ending", (req, res) => {
+// router.get("/game/escape", (req, res) => {
+//   res.render("escape-room", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
+
+router.get("/game/ending/:id", async (req, res) => {
+  const playerData = await Player.findByPk(req.params.id);
+  const player = playerData.get({ plain: true });
   res.render("ending", {
+    player,
     logged_in: req.session.logged_in,
   });
-});
+}); //swap with bottom if not work :PP
+
+// router.get("/game/ending", (req, res) => {
+//   res.render("ending", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
 
 module.exports = router;
--- public/js/roomDungeon.js
+++ public/js/roomDungeon.js
@@ -7,6 +7,9 @@ var ChText3 = document.querySelector("#choice3");
 var ChText4 = document.querySelector("#choice4");
 var nar = document.querySelector("#nar");
 
+const history = document.querySelector("#action-history");
+const inv = document.querySelector("#inv");
+
 let position = 1;
 let neutral = 1;
 let desk = 2;
@@ -17,12 +20,13 @@ let haskey = false;
 let hashammer = false;
 let readPaper = false;
 
-//giving items and changeing choice
+//giving items and changing choice
 
 const choice1 = async (event) => {
   event.preventDefault();
+
   if (position == neutral) {
-    //losading table
+    //loading table
     ChText1.textContent = "Take the hammer.";
     ChText2.classList.add("d-none");
     ChText3.classList.add("d-none");
@@ -30,6 +34,9 @@ const choice1 = async (event) => {
     ChText4.textContent = "Return to main spot.";
     nar.textContent =
       "You walk up to the wooden table and see an arrangement of objects on the wooden table. To the left you see a couple of bones and a jar with orange liquid in it. To the right you see some tools; many are unrecognizable and weirdly shaped. You are able to make out a knife. In the middle of the table you see a glass ball and a piece of paper with something written on it.";
+    let action = document.createElement("p");
+    history.appendChild(action);
+    action.textContent = "Walked to the table."; //add to history
     position = desk;
   }
   //choice 1 of table
@@ -37,6 +44,13 @@ const choice1 = async (event) => {
     //give hammer
     if (hashammer) {
       nar.textContent = "You picked up the hammer.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Picked up hammer!"; //add to history
+      let obtain = document.createElement("img");
+      inv.appendChild(obtain);
+      obtain.setAttribute("src", "/images/items/hammer.png"); //add inventory
+      ChText1.classList.add("d-none"); //removed the take hammer option after picking it up :PP
     }
     hashammer = true;
   }
@@ -44,15 +58,25 @@ const choice1 = async (event) => {
   if (position == door) {
     if (!haskey) {
       nar.textContent = "The door is locked.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Tried opening locked door..."; //add to history
     }
     if (haskey) {
-      document.location.replace("/game/library");
+      const currentLocation = window.location.href;
+      const url = new URL(currentLocation);
+      const id = url.searchParams.get("id") || url.pathname.split("/").pop();
+
+      document.location.replace(`/game/library/${id}`);
     }
   }
   //choice 1 of chest
   if (position == chest) {
     if (!hashammer && !isOpen) {
       nar.textContent = "The chest is locked.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Tried opening locked chest..."; //add to history
     }
     if (hashammer && !isOpen) {
       ChText1.textContent = "Look at the stack of paper";
@@ -61,11 +85,17 @@ const choice1 = async (event) => {
       isOpen = true;
       nar.textContent =
         "You grasp the hammer tightly and repeatedly smash the chain around the chest, until it yields. Succumbing to the relentless assault. It falls away clattering on the stone floor. Upon opening the chest it contains a mess of written pages and scrawled notes.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Smashed chain with hammer!"; //add to history
     }
     if (isOpen == true) {
       if (readPaper) {
         nar.textContent = `Skimming through the lines on the various pages, you are unable to find anything of value.
 However, a single torn sheet of parchment catches your eye. It reads: "He who wishes to be free must find the magic keys three".`;
+        let action = document.createElement("p");
+        history.appendChild(action);
+        action.textContent = "Learned about the three magic keys."; //add to history
       }
       readPaper = true;
     }
@@ -74,6 +104,7 @@ However, a single torn sheet of parchment catches your eye. It reads: "He who wi
 
 const choice2 = async (event) => {
   event.preventDefault();
+
   //loading door
   if (position == neutral) {
     ChText1.textContent = "Open the door.";
@@ -82,6 +113,9 @@ const choice2 = async (event) => {
     ChText4.classList.remove("d-none");
     ChText4.textContent = "Return to main spot.";
     nar.textContent = "You walk over to the door. It has a keyhole.";
+    let action = document.createElement("p");
+    history.appendChild(action);
+    action.textContent = "Walked to the door."; //add to history
     position = door;
   }
   if (position == desk) {
@@ -92,28 +126,44 @@ const choice2 = async (event) => {
     //give key
     haskey = true;
     nar.textContent = "You grabbed the key.";
+    let action = document.createElement("p");
+    history.appendChild(action);
+    action.textContent = "Grabbed a key!"; //add to history
+    let obtain = document.createElement("img");
+    inv.appendChild(obtain);
+    obtain.setAttribute("src", "/images/items/key.png"); //add inventory
   }
 };
 
 const choice3 = async (event) => {
   //loading chest
   event.preventDefault();
+
   if (position == neutral) {
     if (!hashammer && !isOpen) {
       ChText1.textContent = "Open chest";
       ChText1.classList.remove("d-none");
       nar.textContent =
         "You walk up to the chest which looks old and rusted. It is chained shut with a small lock on it.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Walked to the chest."; //add to history
     }
     if (hashammer && !isOpen) {
       nar.textContent =
         "You walk up to the chest which looks old and rusted. It is chained shut with a small lock on it.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Walked to the chest."; //add to history
       ChText1.textContent = "Open chest with the hammer";
       ChText1.classList.remove("d-none");
     }
     if (isOpen == true) {
       nar.textContent =
         "You walk up to the chest which looks old and rusted. It is chained shut with a small lock on it.";
+      let action = document.createElement("p");
+      history.appendChild(action);
+      action.textContent = "Walked to the chest."; //add to history
       ChText1.textContent = "Look through stack of papers";
       ChText1.classList.remove("d-none");
     }
@@ -127,6 +177,7 @@ const choice3 = async (event) => {
 
 const choice4 = async (event) => {
   event.preventDefault();
+
   //loading neutral
   if (position == desk) {
     ChText1.textContent = "Go to the table";
