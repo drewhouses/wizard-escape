b940d19e4ee2f80ec49b22043de0ce63bcbe672c
drewhouses <drewcasas@live.com>
2023-12-06 20:04:40 -0600

    added images to project and added to img tags

--- public/js/typewriter.js
+++ public/js/typewriter.js
@@ -1,8 +1,8 @@
 const intro = new Typed("#intro-text", {
   strings: [
-    "As I ventured into the heart of the foreboding woods, the thick canopy overhead swallowed the feeble moonlight, casting an eerie gloom that clung to every gnarled branch and twisted root. The trees, standing like silent sentinels, leaned in towards the narrow trail, their skeletal branches interlocking to form a natural barrier. Shadows danced between the trunks, playing tricks on my imagination. As I moved deeper, the path began to twist and turn unpredictably, and the skeletal fingers of branches reached out as if to grab hold of any who dared to intrude upon their realm. Suddenly I hear a swift approach from behind as a loud thump collapses me to the ground. A sharp pain in the back of my skull, the taste of dirt as consciousness fades.",
+    "As you venture into the heart of the foreboding woods, the thick canopy overhead swallowed the feeble moonlight, casting an eerie gloom that clung to every gnarled branch and twisted root. The trees, standing like silent sentinels, leaned in towards the narrow trail, their skeletal branches interlocking to form a natural barrier. Shadows danced between the trunks, playing tricks on my imagination. As you move deeper, the path began to twist and turn unpredictably, and the skeletal fingers of branches reached out as if to grab hold of any who dared to intrude upon their realm. Suddenly, you hear a swift approach from behind as a loud thump collapses you to the ground. A sharp pain in the back of your skull, the taste of dirt as consciousness fades.",
   ],
-  typeSpeed: 50,
+  typeSpeed: 20,
 });
 
 // const start = new Typed("#nar", {
